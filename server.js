7f601f1e0975ecb9d54e44096c37b4800e0d4617
ArhumTheDeveloper <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-07-17 16:35:06 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+import 'dotenv'.config(); from 'dotenv'
+const express "express";
+import http from "http";
+const app = express();
+const server = http.createServer(app);
+import socket from "socket.io";
+const io = socket(server);
+
+const users = {};
+
+const socketToRoom = {};
+
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+    });
+
+});
+
+server.listen(process.env.PORT || 3000, () => console.log('server is running on port 3000'));
+
+module.exports = app;
