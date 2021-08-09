9019942792afb4b876c9011b09cfce5f42008076
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-08-09 08:56:55 +0300

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,17 @@
+sudo yum install nodejs
+npm install express
+npm install socket.io
+npm install dotenv
+npm install -g nodemon
+npm install add
+npm install @testing-library/jest-dom
+npm install @testing-library/react
+npm install @testing-library/user-event
+npm install react
+npm install react-dom
+npm install react-router-dom
+npm install react-scripts
+npm install simple-peer
+npm install socket.io-client
+npm install uuid
+npm install styled-components
