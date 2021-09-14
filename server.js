2e805247f0fb39193c2ce96f3b0b485bf8d78d37
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-09-14 16:12:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-import 'dotenv'.config(); from 'dotenv'
-const express "express";
-import http from "http";
+require('dotenv').config();
+var express = require("express")
+var http = require("http");
 const app = express();
 const server = http.createServer(app);
-import socket from "socket.io";
+var socket = require("socket.io");
 const io = socket(server);
 
 const users = {};
 
 const socketToRoom = {};
 
-io.on('connection', socket => {
-    socket.on("join room", roomID => {
-        if (users[roomID]) {
+io.on('connection', socket =>
+{
+    socket.on("join room", roomID =>
+    {
+        if (users[roomID])
+        {
             const length = users[roomID].length;
-            if (length === 4) {
+            if (length === 4)
+            {
                 socket.emit("room full");
                 return;
             }
             users[roomID].push(socket.id);
-        } else {
+        } else
+        {
             users[roomID] = [socket.id];
         }
         socketToRoom[socket.id] = roomID;
@@ -28,18 +33,22 @@ io.on('connection', socket => {
         socket.emit("all users", usersInThisRoom);
     });
 
-    socket.on("sending signal", payload => {
+    socket.on("sending signal", payload =>
+    {
         io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
     });
 
-    socket.on("returning signal", payload => {
+    socket.on("returning signal", payload =>
+    {
         io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
     });
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', () =>
+    {
         const roomID = socketToRoom[socket.id];
         let room = users[roomID];
-        if (room) {
+        if (room)
+        {
             room = room.filter(id => id !== socket.id);
             users[roomID] = room;
         }
