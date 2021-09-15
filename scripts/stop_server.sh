b447989d83aac39ec215a08f2b0873f23f154cde
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-09-15 21:19:04 +0300

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,1 @@
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-cat /etc/system-release
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo systemctl stop httpd
+sudo pkill -f node
