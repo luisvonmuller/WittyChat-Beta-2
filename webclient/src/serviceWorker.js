6ee94e10e653294c1153f8f87081f16243fc73e1
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-10-02 14:22:57 +0300

    Update serviceWorker.js

--- webclient/src/serviceWorker.js
+++ webclient/src/serviceWorker.js
@@ -18,7 +18,7 @@ export function register(config)
 
     window.addEventListener('load', (checkValidServiceWorker) =>
     {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/webclient/src/service-worker.js`;
 
       if (isLocalhost)
       {
