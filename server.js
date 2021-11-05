a35dffa010c70ae8755ec07c72225b988c164e11
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-11-05 14:12:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ module.exports = app;
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static(__dirname,'/webclient/src/room/room.js'));
+  app.use(express.static('src'));
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '/index.html'));
