176d97b3e380071e73839aa657f15bb4d8d870b8
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-11-05 14:19:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ module.exports = app;
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('src'));
+  app.use(express.static(__dirname, '/src'));
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '/index.html'));
