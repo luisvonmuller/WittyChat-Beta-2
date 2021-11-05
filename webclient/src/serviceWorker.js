4d06d8811dbbc982b6910ef506a0ccbb28ddd9ce
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-11-05 16:51:57 +0300

    notfix4

--- webclient/src/serviceWorker.js
+++ webclient/src/serviceWorker.js
@@ -85,39 +85,39 @@ function registerValidSW(swUrl, config)
     });
 }
 
-// function checkVaildServiceWorker(swUrl, config) 
-// {
-//   const contentType = response.headers.get('content-type');
-//   if (
-//     response.status === 404 ||
-//     (contentType != null && contentType.indexOf('javascript') === -1)
-//   ) 
-//   {
-//     navigator.serviceWorker.ready.then(registration =>
-//     {
-//       try
-//       {
-//         registration.unregister().then(() =>
-//         {
-//           window.location.reload();
-//         }
-//         );
-//       } catch (error)
-//       {
-//         (() =>
-//         {
-//           console.log(
-//             'No internet connection found. App is running in offline mode.'
-//           );
-//         });
-//       }
+ function checkVaildServiceWorker(swUrl, config) 
+ {
+   const contentType = response.headers.get('content-type');
+   if (
+     response.status === 404 ||
+     (contentType != null && contentType.indexOf('javascript') === -1)
+   ) 
+   {
+    navigator.serviceWorker.ready.then(registration =>
+     {
+       try
+      {
+         registration.unregister().then(() =>
+        {
+           window.location.reload();
+         }
+         );
+      } catch (error)
+      {
+         (() =>
+         {
+           console.log(
+             'No internet connection found. App is running in offline mode.'
+           );
+         });
+       }
 
-//     });
-//   } else
-//   {
-//     registerValidSW(swUrl, config);
-//   }
-// }
+     });
+   } else
+   {
+     registerValidSW(swUrl, config);
+   }
+ }
 
 
 export function unregister()
