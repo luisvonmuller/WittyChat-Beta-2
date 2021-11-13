6638438cf7056202c54989887b57fac88c59419a
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-11-13 19:50:39 +0300

    fix 40

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require("express")
 var router = express.Router();
 var http = require("http");
 const app = express();
-var cors = require('cors')
 const server = http.createServer(app);
 var socket = require("socket.io");
 const io = socket(server);
@@ -60,14 +59,7 @@ io.on('connection', socket =>
 
 server.listen(process.env.PORT || 3000, () => console.log('server is running on port 3000'));
 
-const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Access-Control-Allow-Origin", "https://example.com/");
-    res.setHeader("Access-Control-Allow-Method", "GET, DELETE, HEAD, OPTIONS, POST,OPTIONS");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.writeHead(200);
-    res.end(``);
-};
+
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
@@ -75,7 +67,7 @@ if (process.env.NODE_ENV === 'production') {
     router.get(path.join(__dirname, 'webclient/src/room/room.js'), (req, res, next) => {
        next()
     });
-        app.use('/webclient/src/room/room.js', cors(requestListener), express.static((__dirname, 'webclient/src/room/room.js')));
+        app.use('/webclient/src/room/room.js',  express.static((__dirname, 'webclient/src/room/room.js')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'webclient/public/index.html'));
 
